c1f8580a67805850273176f815940c26a67583d3
AADHI VINAY <103887249+aadhivinay@users.noreply.github.com>
2024-09-24 11:32:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function moveCarousel(direction, carouselClass) {
   const cardContainer = document.querySelector(`.${carouselClass}`);
   const cards = cardContainer.querySelectorAll('.card');
   const totalSlides = cards.length;
+  
+ 
 
   let currentSlide = parseInt(cardContainer.dataset.currentSlide) || 0;
   currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
@@ -17,6 +19,23 @@ function moveCarousel(direction, carouselClass) {
     const cardWidth = cards[0].offsetWidth + 20; // Adjusted for card width and margin
     cardContainer.style.transform = `translateX(-${currentSlide * cardWidth}px)`;
   }
+  
 
   cardContainer.dataset.currentSlide = currentSlide;
 }
+// Function to show the greeting pop-up
+function showGreetingPopup() {
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+  popup.innerHTML = `
+    <p>Hey ✌️ Welcome To My Page .. </p>
+    <button class="close-button">Close</button>
+  `;
+  document.body.appendChild(popup);
+  const closeButton = popup.querySelector('.close-button');
+  closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+}
+
+window.addEventListener('load', showGreetingPopup);
